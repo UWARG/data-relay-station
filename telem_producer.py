3338bea0f4ea205000881455ae2f8b3565788508
Andrew Crichton <andrew.g.crichton@gmail.com>
2014-02-15 14:11:23 -0500

    add empty telem producer

---
+++ telem_producer.py
@@ -0,0 +1,17 @@
+
+
+class TelemProducer(object):
+    """Produce the """
+
+    def __init__(self, proto):
+        self._proto = proto
+        self._paused = False
+
+    def pauseProducing(self):
+        pass
+
+    def resumeProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
