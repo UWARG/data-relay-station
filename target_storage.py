21c9918327dd1e7d57e8ede2687face31e68e8d2
Andrew Crichton <andrew.g.crichton@gmail.com>
2015-04-21 02:49:55 -0400

    Argparse and set history size.

--- target_storage.py
+++ target_storage.py
@@ -4,7 +4,7 @@ import argparse
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
 
-MAX_HISTORY_SIZE = 200
+DEFAULT_MAX_HISTORY_SIZE = 200
 
 class MultiEcho(Protocol):
 
@@ -27,20 +27,22 @@ class MultiEcho(Protocol):
 
 class MultiEchoFactory(Factory):
 
-    def __init__(self):
+    def __init__(self, history_size = DEFAULT_MAX_HISTORY_SIZE):
         self.echoers = []
-        self.history = deque(maxlen = MAX_HISTORY_SIZE)
+        self.history = deque(maxlen = history_size)
 
     def buildProtocol(self, addr):
         return MultiEcho(self)
 
 
-def main():
-    reactor.listenTCP(4321, MultiEchoFactory())
+def main(history_size):
+    reactor.listenTCP(4321, MultiEchoFactory(history_size))
     reactor.run()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A server to re-echo any data sent to it, as well as the history of all data for this session.")
+    parser = argparse.ArgumentParser(description=
+            """A server to re-echo any data sent to it, as well as the history of all data for this session.""")
+    parser.add_argument("--max_history", metavar="N", required=False, help="maximum history size for this server to store (default: 200)", default=DEFAULT_MAX_HISTORY_SIZE)
     args = parser.parse_args()
-    main()
+    main(args.max_history)
