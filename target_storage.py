d1963dded808aeb9abd8bc4f91763330a8f69019
Andrew Crichton <andrew.g.crichton@gmail.com>
2015-04-21 02:32:02 -0400

    A working implementation.

---
+++ target_storage.py
@@ -0,0 +1,43 @@
+import argparse
+
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+
+
+class MultiEcho(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        for history_line in self.factory.history:
+            self.transport.write(history_line)
+        self.factory.echoers.append(self)
+
+    def dataReceived(self, data):
+        self.factory.history.append(data)
+        for echoer in self.factory.echoers:
+            echoer.transport.write(data)
+
+    def connectionLost(self, reason):
+        self.factory.echoers.remove(self)
+
+
+class MultiEchoFactory(Factory):
+
+    def __init__(self):
+        self.echoers = []
+        self.history = []
+
+    def buildProtocol(self, addr):
+        return MultiEcho(self)
+
+
+def main():
+    reactor.listenTCP(4321, MultiEchoFactory())
+    reactor.run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    main()
