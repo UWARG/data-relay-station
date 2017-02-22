21675947898464f243cfbdfec3d35c775c70d510
Stephen Cholvat <stephen.cholvat@gmail.com>
2017-02-21 22:25:49 -0500

    cleaned up receiver

--- receiver.py
+++ receiver.py
@@ -82,123 +82,65 @@ class Receiver:
         """
         self.outbound.append(command)
 
-
-    def reconnect_xbee(self):
-    #search for available ports
-        port_to_connect = ''
-        while port_to_connect == '':
-
-            #detect platform and format port names
-            if _platform.startswith('win'):
-                ports = ['COM%s' % (i + 1) for i in range(256)]
-            elif _platform.startswith('linux'):
-                # this excludes your current terminal "/dev/tty"
-                ports = glob.glob('/dev/ttyUSB*')
-            else:
-                raise EnvironmentError('Unsupported platform: ' + _platform)
-
-            ports_avail = []
-
-            #loop through all possible ports and try to connect
-            for port in ports:
-                try:
-                    s = serial.Serial(port)
-                    s.close()
-                    ports_avail.append(port)
-                except (OSError, serial.SerialException):
-                    pass
-
-            if len(ports_avail) ==1:
-                port_to_connect = ports_avail[0]
-
-            elif len(ports_avail)==0:
-                #No Serial port found, continue looping.
-                print( "No serial port detected. Trying again...")
-                time.sleep(1)
-
-            elif len(ports_avail)>1:
-                #Multiple serial ports detected. Get user input to decide which one to connect to
-                #com_input = raw_input("Multiple serial ports available. Which serial port do you want? \n"+str(self.ports_avail)+":").upper();
-                if self.default_serial == None:
-                    raise EnvironmentError('Incorrect command line parameters. If there are multiple serial devices, indicate what port you want to be used using --serialport')
-                elif self.default_serial.upper() in ports_avail:
-                    port_to_connect = self.default_serial.upper()
-                else:
-                    raise EnvironmentError('Incorrect command line parameters. Serial port is not known as a valid port. Valid ports are:'+ str(ports_avail))
-
-        #connect to xbee
-        self.xbee = ZigBee(serial.Serial(port_to_connect, 115200))
-        print('xbee connected to port ' + port_to_connect)
-        return self
-
-
-    def __enter__(self):
-        return self
-
-
+    #TODO make async
     def data_lines(self):
 
         #counter to limit extra packets sent
         packetCnt=0
         while True:
-            try:
-                payload = ''
-                yield_data = ()
-                for x in xrange(self.expected_packets):
+            payload = ''
+            yield_data = ()
+            for x in xrange(self.expected_packets):
 
+                packet = self.xbee.wait_read_frame()
+
+                #limit packets by only sending decibel strength only when if statement is true
+                if(packetCnt>=10):
+                    self.xbee.at(command="DB")
+                    packetCnt=0
+                packetCnt=packetCnt+1
+
+                while packet.get('id', None) != 'rx':
+                    #Checks for tx_response
+                    if packet.get('id', None) == 'tx_status':
+                        print('got tx_status frame')
+                    #Checks for command response and signal strength
+                    elif packet.get('id', None) == 'at_response':
+                        if packet.get('command', None) == 'DB':
+                            self.rssi = ord(packet.get('parameter',self.rssi))
                     packet = self.xbee.wait_read_frame()
 
-                    #limit packets by only sending decibel strength only when if statement is true
-                    if(packetCnt>=10):
-                        self.xbee.at(command="DB")
-                        packetCnt=0
-                    packetCnt=packetCnt+1
+                self.source_addr_long = packet.get(
+                        'source_addr_long', self.source_addr_long)
+                self.source_addr = packet.get(
+                        'source_addr', self.source_addr)
 
-                    while packet.get('id', None) != 'rx':
-                        #Checks for tx_response
-                        if packet.get('id', None) == 'tx_status':
-                            print('got tx_status frame')
-                        #Checks for command response and signal strength
-                        elif packet.get('id', None) == 'at_response':
-                            if packet.get('command', None) == 'DB':
-                                self.rssi = ord(packet.get('parameter',self.rssi))
-                        packet = self.xbee.wait_read_frame()
+                payload += packet['rf_data']
 
-                    self.source_addr_long = packet.get(
-                            'source_addr_long', self.source_addr_long)
-                    self.source_addr = packet.get(
-                            'source_addr', self.source_addr)
+                # Read first two bytes, to determine packet type
+                packet_type = struct.unpack("h", payload[:2])[0]
 
-                    payload += packet['rf_data']
+                # Unpack Struct according to ID, and update global parameters
+                for data_type, data_shape in self.data_shape.iteritems():
+                    if (packet_type == data_type):
+                        self.stored_data[data_type] = data_shape.unpack(payload[2:])
+                    else:
+                        self.stored_data[data_type] = tuple([None] * len([i for i in data_shape.format if i != 'x']))
+                yield_data = tuple([i for j in self.stored_data for i in j])
 
-                    # Read first two bytes, to determine packet type
-                    packet_type = struct.unpack("h", payload[:2])[0]
+                #Add RSSI to each packet
+                yield_data += tuple([self.rssi])
 
-                    # Unpack Struct according to ID, and update global parameters
-                    for data_type, data_shape in self.data_shape.iteritems():
-                        if (packet_type == data_type):
-                            self.stored_data[data_type] = data_shape.unpack(payload[2:])
-                        else:
-                            self.stored_data[data_type] = tuple([None] * len([i for i in data_shape.format if i != 'x']))
-                    yield_data = tuple([i for j in self.stored_data for i in j])
+            # let our data be processed - unpacks an array of tuples into one single tuple
+            yield yield_data
 
-                    #Add RSSI to each packet
-                    yield_data += tuple([self.rssi])
-
-                # let our data be processed - unpacks an array of tuples into one single tuple
-                yield yield_data
-
-                # flush the command queue to the xbee
-                for cmd in self.outbound:
-                    self.xbee.tx(dest_addr_long=self.source_addr_long,
-                            dest_addr=self.source_addr, data=cmd)
-                    print("command {}".format(' '.join("0x{:02x}".format(i) for i in cmd)))
-                    print("sent a command")
-                self.outbound = []
-            except (OSError, serial.SerialException, IOError):
-                #catch exception if xbee is unplugged, and try to reconnect
-                print("Xbee disconnected!")
-                #self.reconnect_xbee()
+            # flush the command queue to the xbee
+            for cmd in self.outbound:
+                self.xbee.tx(dest_addr_long=self.source_addr_long,
+                        dest_addr=self.source_addr, data=cmd)
+                print("command {}".format(' '.join("0x{:02x}".format(i) for i in cmd)))
+                print("sent a command")
+            self.outbound = []
 
 
     def __exit__(self, type, value, traceback):
--- relay_station.py
+++ relay_station.py
@@ -2,18 +2,21 @@ from data_relay import DataRelay
 
 
 def main(simfile, simspeed):
-    relay = DataRelay(simfile, simspeed)
+
     #initialize UDP connections
+    #TODO
+
     #start data relay
+    relay = DataRelay(simfile, simspeed)
+
     #start multi echo
+    #TODO
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Read data from xbee, write it locally and replay it over the network to connected clients.")
     parser.add_argument("--simfile", metavar="FILE", required=False, help="list of files (separated by commas) to simulate incoming plane data")
     parser.add_argument("--simspeed", metavar="NUMBER", required=False, help="speed to play the simfile at in seconds per frame")
-    parser.add_argument("--log", action='store_true', help="Always write log file (even in simulator mode).")
-    parser.add_argument("--nolog", action='store_true', help="Never write log file.")
     args = parser.parse_args()
     #Default Sim Speed
     simspeed = 0.2
